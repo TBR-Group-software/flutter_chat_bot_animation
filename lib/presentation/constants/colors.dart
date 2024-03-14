fcea774c2f3ad3e788f2375ba2be2d85ca11c253
Kyrylo Petrenko <petrs911@gmail.com>
2024-03-14 17:15:51 +0200

    added assets to the app

---
+++ lib/presentation/constants/colors.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+abstract class ProjectColors {
+  static const white5 = Color(0x0DFFFFFF);
+
+  static const electricBlue = Color(0xFF7000FF);
+
+  static const magenta = Color(0xFFFF00B8);
+
+  static const midnightBlue = Color(0xFF3C00E8);
+
+  static const midnightBlue40 = Color(0x663C00E8);
+
+  static const hotPink = Color(0xFFFA00FF);
+
+  static const hotPink20 = Color(0x33FA00FF);
+
+  static const lightGrey = Color(0xFFC3C3C3);
+
+  static const darkGray = Color(0xFF646464);
+
+  static const lavender = Color(0xFF9643FF);
+
+  static const lavender80 = Color(0xCC9643FF);
+
+  static const purpleShade = Color(0xFFB384FF);
+
+  static const purpleShade80 = Color(0xCCB384FF);
+
+  static const charcoal = Color(0xFF2E2E2E);
+}
---
+++ lib/presentation/constants/text_style.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+import 'gen/fonts.gen.dart';
+
+abstract class ProjectTextStyle {
+  static const _chivoFontFamily = ProjectFontFamily.chivo;
+  static const _comfortaaFontFamily = ProjectFontFamily.comfortaa;
+
+  static const chivoRegular10DarkGrey = TextStyle(
+    fontFamily: _chivoFontFamily,
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: ProjectColors.darkGray,
+  );
+
+  static const chivoRegular12White = TextStyle(
+    fontFamily: _chivoFontFamily,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static const chivoRegular13LightGrey = TextStyle(
+    fontFamily: _chivoFontFamily,
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: ProjectColors.lightGrey,
+  );
+
+  static const chivoRegular14LightGrey = TextStyle(
+    fontFamily: _chivoFontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: ProjectColors.lightGrey,
+  );
+
+  static const chivoRegular16White = TextStyle(
+    fontFamily: _chivoFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static const chivoRegular20White = TextStyle(
+    fontFamily: _chivoFontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static const comfortaaMedium24White = TextStyle(
+    fontFamily: _comfortaaFontFamily,
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+}
